cdf8b178f0fe0c8d40d8de043d282556eeba5062
Manasa_Rani_M <manasa.r@polynomial.ai>
2022-05-06 10:18:00 +0530

    update mail and service configs

--- src/config/config.js
+++ src/config/config.js
@@ -6,11 +6,18 @@ let credentials = {
 	// 	email: "jessica.bot.contact@gmail.com",
 	// 	url: "https://developers.google.com/oauthplayground",
 	// },
+	// mailCreds : {
+	// 	clientId: "295325884798-ar1js47jkdtr2u4jfliegsc2sue8fef3.apps.googleusercontent.com",
+	// 	clientSecret: "5x-K9v-3qhDXPUqCqqWEctuE",
+	// 	refreshToken: "1//04yL9gV6Q_FHPCgYIARAAGAQSNwF-L9IrjuIASdbd1TG_OKM0WqyvLYvWKzXccmjOvG44urwlpUZigIKMkkT5DvxNL0lFSHSypMg",
+	// 	email: "presales@kanerika.com",
+	// 	url: "https://developers.google.com/oauthplayground",
+	// }
 	mailCreds : {
-		clientId: "295325884798-ar1js47jkdtr2u4jfliegsc2sue8fef3.apps.googleusercontent.com",
-		clientSecret: "5x-K9v-3qhDXPUqCqqWEctuE",
-		refreshToken: "1//04yL9gV6Q_FHPCgYIARAAGAQSNwF-L9IrjuIASdbd1TG_OKM0WqyvLYvWKzXccmjOvG44urwlpUZigIKMkkT5DvxNL0lFSHSypMg",
-		email: "presales@kanerika.com",
+		clientId: "362207612748-fgkpbpnkp41m2jitruvt6st36eat26h6.apps.googleusercontent.com",
+		clientSecret: "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",
+		refreshToken: "1//04vV8nJQDUuM4CgYIARAAGAQSNwF-L9Ir1jNoNHmyyfYisji-N_l7DtriEs01yBPUWXLZwI0zPx-S3IPeuAg1kLAvz31lXCaw9dY",
+		email: "bot.leadmanager@gmail.com",
 		url: "https://developers.google.com/oauthplayground",
 	}
 };
