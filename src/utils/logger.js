fe11ee35ee729d163d38bf6b06fb34aae49f222f
Manasa_Rani_M <manasa.r@polynomial.ai>
2022-05-06 11:56:22 +0530

    lead management bug fixes

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -108,7 +108,7 @@ async function leadGenaratedLogs(conversationData) {
 		};
 		await doc.loadInfo();
 		const sheet = doc.sheetsByIndex[0];
-		// console.log({logger,sheet})
+		console.log({logger,sheet})
 		sheet.addRow(logger);	
 	}catch(error){
 		console.log({error})
