12939280295f578b861cdd240f60a160f2355955
artiborad <artiborad0812@gmail.com>
2022-05-19 11:11:17 +0530

    initial commit

--- src/utils/mailComposer.js
+++ src/utils/mailComposer.js
@@ -46,7 +46,7 @@ function mailComposerForLink(userDetails, email, solutionName) {
 		subject: `Please find the more details for the requested info:`,
 		body: `Hi,<br><br>
               Please find the requested link attached.<br>
-              ${slotValues.details.urlToBeEmailed}<br><br>
+              ${userDetails.urlToBeEmailed}<br><br>
               Best Regards,<br>
               Online 24*7`,
 		// body: `Hi ${name},<br><br>
